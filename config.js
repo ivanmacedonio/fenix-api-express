4ce07cf977a3fbe62f807613bc475168ea30c369
ivanmacedonio778 <ivanmacedonio778@gmail.com>
2024-02-23 17:49:33 -0300

    change database

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 const dbconnect = () => {
     mongoose.set('strictQuery', true)
-    mongoose.connect('mongodb://localhost:27017/fenix_ecommerce')
+    mongoose.connect('mongodb+srv://ivanmacedonio778:mamageor28@fenix.qcbsftl.mongodb.net/')
         .then(()=> {
             console.log('Conexion existosa')
         })
