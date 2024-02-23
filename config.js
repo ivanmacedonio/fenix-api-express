73a0ee215febe7f83e6aa5cab2dc7154b9b70170
ivanmacedonio778 <ivanmacedonio778@gmail.com>
2024-02-23 18:02:01 -0300

    change db

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 const dbconnect = () => {
     mongoose.set('strictQuery', true)
-    mongoose.connect('mongodb+srv://ivanmacedonio778:mamageor28@fenix.qcbsftl.mongodb.net/')
+    mongoose.connect('mongodb+srv://ivanmacedonio778:mamageor28@fenix.qcbsftl.mongodb.net/?retryWrites=true&w=majority&appName=fenix')
         .then(()=> {
             console.log('Conexion existosa')
         })
@@ -12,3 +12,4 @@ const dbconnect = () => {
 }
 
 export default dbconnect
+
